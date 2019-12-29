5df977a807ceaf152f31edca774563a6b4c90720
upbound-web <jake@upbound.com.au>
2019-12-30 07:15:07 +1100

    changed link to generic service m8 link

--- src/pages/Components/Sections/Services.jsx
+++ src/pages/Components/Sections/Services.jsx
@@ -39,7 +39,7 @@ class Services extends React.Component {
                                     <h2>Domestic Cleaning</h2>
                                     <p>Have one of our highly skilled team come out to your home and give you a professional clean leaving the home sparkling. </p>
                                     <Button 
-                                    href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b#25b2c9b8-2042-4f95-851d-2586c7e6e5eb" 
+                                    href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b" 
                                     color= "primary"> Book Now </Button>
                                 </div>
                             </div>
@@ -56,7 +56,7 @@ class Services extends React.Component {
                                     <h2>Commercial Clean</h2>
                                     <p>Have one of our fast and efficient team come to your office or work site to get it sparkling, from spring cleans to regular cleaning services we have you covered.</p>
                                     <Button
-                                     href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b#3372478c-a8bd-4279-a8b0-e2674a3c374b"
+                                     href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b"
                                      color= "primary"> Book Now </Button>
                                 </div>
                             </div>
@@ -73,7 +73,7 @@ class Services extends React.Component {
                                     <h2>Ironing and Washing</h2>
                                     <p>Need ironing, washing, changing the sheets, making beds? Any sort of house keeping to make your life simpler. Let us take care of it for you</p>
                                     <Button 
-                                    href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b#68d3e6e2-8c20-454a-b9a0-cd9cb02a496b"
+                                    href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b"
                                     color= "primary"> Book Now </Button>
                                 </div>
                             </div>
@@ -90,8 +90,8 @@ class Services extends React.Component {
                                     <h2>Mowing and Garden Maintenance</h2>
                                     <p>Mowing, weeding, pruning, wiper snipping, green waste removed. A regular service or just the once</p>
                                     <Button 
-                                    href="#contact"
-                                    color= "primary"> Contact Now </Button>
+                                    href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b"
+                                    color= "primary"> Book Now </Button>
                                 </div>
                             </div>
                         </div>
@@ -107,7 +107,7 @@ class Services extends React.Component {
                                     <h2>Builders Clean</h2>
                                     <p>Builders / Contractors need your site cleaned to handover to the owner? A home, block of units or major construction, we hand your site over to your customers requirements.</p>
                                     <Button 
-                                    href= "https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b#b6f038cd-8a2b-443a-8452-8e9b47c66c8b" 
+                                    href= "https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b" 
                                     color= "primary"> Book Now </Button>
                                 </div>
                             </div>
@@ -124,8 +124,8 @@ class Services extends React.Component {
                                     <h2>Proffesional Window Clean</h2>
                                     <p>Have one of our highly skilled team come out to your home or office & give your windows a professional clean leaving them sparkling. </p>
                                     <Button 
-                                    href="#contact"
-                                    color= "primary"> Contact Now </Button>
+                                    href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b"
+                                    color= "primary"> Book Now </Button>
                                 </div>
                             </div>
                         </div>
@@ -141,10 +141,10 @@ class Services extends React.Component {
                                     <h2>Rubbish Removal</h2>
                                     <p>We remove rubbish, whatever you need gone we will remove it for you. We will take care of everything for you.</p>
                                     <Button 
-                                    href="#contact"
+                                    href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b"
                                     color= "primary"
                                     > 
-                                        Contact Now 
+                                        Book Now 
                                     </Button>    
                                 </div>
                             </div>
@@ -161,8 +161,8 @@ class Services extends React.Component {
                                     <h2>Solar Panel Clean</h2>
                                     <p>You often wont realise how dirty your solar pannels are and the effect that can have on their efficiency. Let us make sure they are sparkling and working at maximum efficiency</p>
                                     <Button 
-                                    href="#contact"
-                                    color= "primary"> Contact Now </Button>    
+                                    href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b"
+                                    color= "primary"> Book Now </Button>    
                                 </div>
                             </div>
                         </div>
@@ -178,8 +178,8 @@ class Services extends React.Component {
                                     <h2>End of Lease Clean</h2>
                                     <p>Want to make sure your place is sparkling before you hand the keys back to the rental agent. Dont risk it. Book us in to take care of it for you.</p>
                                     <Button 
-                                    href="#contact"
-                                    color= "primary"> Contact Now </Button>    
+                                    href="https://book.servicem8.com/request_service_booking?strVendorUUID=f588290e-1fea-44d5-b231-2dfe4adc487b"
+                                    color= "primary"> Book Now </Button>    
                                 </div>
                             </div>
                         </div>
