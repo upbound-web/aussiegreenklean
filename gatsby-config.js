67ad3ea700a09f612d1e9c1e42e1b7c2102431c4
Jake Dawson <jake@upbound.com.au>
2020-02-13 08:45:46 +1100

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,23 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/assets/img/`
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-156924743-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
   ],
 }
